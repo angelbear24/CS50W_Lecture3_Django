894f4bdd7099099d8face3aec2dafc027a0c9ee4
angelbear24 <angelbear24@ymail.com>
2022-05-01 10:21:30 -0500

    Imported Django forms

--- lecture3/tasks/views.py
+++ lecture3/tasks/views.py
@@ -1,6 +1,16 @@
+from audioop import reverse
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django import forms
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
-tasks = ["foo", "bar", "baz"]
+tasks = []
+
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="New Task")
+    #priority = forms.IntegerField(label="Priority", min_value=1, max_value=5)
+
 # Create your views here.
 def index(request):
     return render(request, "tasks/index.html", {
@@ -8,4 +18,17 @@ def index(request):
     })
 
 def add(request):
-    return render(request, "tasks/add.html")
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data["task"]
+            tasks.append(task)
+            return HttpResponseRedirect(reverse("tasks:index"))
+        else:
+            return render(request, "tasks/add.html", {
+                "form": form
+            })
+
+    return render(request, "tasks/add.html", {
+        "form": NewTaskForm()
+    })
